041379d4236fa70abe4a244e57eb1884f4498fc9
Jeff Forshée <jeffrey.forshee@gmail.com>
2024-01-18 20:18:05 -0600

    Mostly working. Still triggering extra times.

--- source.c
+++ source.c
@@ -11,29 +11,24 @@
 #define LED_4 21
 #define LED_PIN PICO_DEFAULT_LED_PIN
 #define TOUCH_PIO pio0
+#define TOUCH_PIN 2 //GPIO number for the first touch button
+#define TOUCH_NUMBER 12 //number of sequential touch buttons
 
-uint touch_state = 0;
+volatile uint touch_state = 0;
 volatile bool touch_change_flg = 0;
 
-
-
-void touch_isr_handeler(void){
-    while (!pio_sm_is_rx_fifo_empty(TOUCH_PIO, 0)){
+void touch_isr_handeler(void){ //This assumes that the state machines are 0-2 in order and that all three are used.
+    if(!pio_sm_is_rx_fifo_empty(TOUCH_PIO, 0)){
         touch_state = (touch_state & 0xffffffe0)|(pio_sm_get(TOUCH_PIO,0));
-        touch_change_flg = 1;
     }
-    while (!pio_sm_is_rx_fifo_empty(TOUCH_PIO, 1)){
+    if(!pio_sm_is_rx_fifo_empty(TOUCH_PIO, 1)){
         touch_state = (touch_state & 0xfffffc1f)|(pio_sm_get(TOUCH_PIO,1)<<5);
-        touch_change_flg = 1;
     }
-    while (!pio_sm_is_rx_fifo_empty(TOUCH_PIO, 2)){
+    if(!pio_sm_is_rx_fifo_empty(TOUCH_PIO, 2)){
         touch_state = (touch_state & 0xffff83ff)|(pio_sm_get(TOUCH_PIO,2)<<10);
-        touch_change_flg = 1;
     }
-    while (!pio_sm_is_rx_fifo_empty(TOUCH_PIO, 3)){
-        touch_state = (touch_state & 0xfff07fff)|(pio_sm_get(TOUCH_PIO,3)<<15);
-        touch_change_flg = 1;
-    }
+    touch_change_flg = 1;
+    //printf("touch_state: %20b isr\n", touch_state);
 }
 
 int touch_setup(PIO pio_touch,int num_buttons, int start_pin, const float clk_div){
@@ -66,14 +61,14 @@ int touch_setup(PIO pio_touch,int num_buttons, int start_pin, const float clk_di
         touch_init(pio_touch, sm, offset_touch, start_pin + 15, (num_buttons-15), clk_div); //get the numbers right
         pio_sm_set_enabled(pio_touch, sm, true);
     }
-//Set up pio interrupts
     irq_set_exclusive_handler(PIO0_IRQ_0,touch_isr_handeler);
+    //irq_add_shared_handler(PIO0_IRQ_0, touch_isr_handeler,PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
     irq_set_enabled(PIO0_IRQ_0, true);   
 }
 
 int main(){
     stdio_init_all();
-    static const float pio_clk_div = 40;
+    static const float pio_clk_div = 40; //was 40, 20 is too sensitive 50 is not sensitive enough
     gpio_init(LED_1);
     gpio_set_dir(LED_1, GPIO_OUT);
     gpio_init(LED_2);
@@ -85,16 +80,16 @@ int main(){
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    touch_setup(TOUCH_PIO, 12, 2, pio_clk_div);
+    touch_setup(TOUCH_PIO, TOUCH_NUMBER, TOUCH_PIN, pio_clk_div);
 
     while (true){
         if(touch_change_flg){
             touch_change_flg = 0;
-            printf("touch_state: %32b \n", touch_state);
-            };
-        gpio_put (LED_1, touch_state&1);
-        gpio_put (LED_2, (touch_state>>1)&1);
-        gpio_put (LED_3, (touch_state>>2)&1);
+            printf("touch_state: %20b \n", touch_state);
+            }
+        gpio_put (LED_1, !pio_sm_is_rx_fifo_empty(TOUCH_PIO, 0));
+        gpio_put (LED_2, !pio_sm_is_rx_fifo_empty(TOUCH_PIO, 1));
+        gpio_put (LED_3, !pio_sm_is_rx_fifo_empty(TOUCH_PIO, 2));
         gpio_put (LED_4, (touch_state>>3)&1);
         gpio_put (LED_PIN, touch_state ? 1 : 0);
     }
